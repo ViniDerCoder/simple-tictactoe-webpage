37933b8bfd0045c38f8f795c939cec55bf785c08
ViniDerCoder <85809337+ViniDerCoder@users.noreply.github.com>
2024-06-19 17:52:46 +0200

    Added score

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -8,6 +8,11 @@ export let currentPlayer = 'X'
 export let computerPlayer = 'O'
 export let humanPlayer = computerPlayer === "X" ? "O" : "X"
 
+export let score = {
+    player: 0,
+    computer: 0
+}
+
 export function startGame() {
     currentPlayer = Math.round(Math.random()) == 1 ? 'X' : 'O'
 
@@ -38,15 +43,47 @@ export function computerMove() {
 
 function move(row, col, player) {
     if(changeField(row, col, player)) {
-        if(checkDraw(getField())) {
+        let winner = checkWin(getField())
+        if(!winner && checkDraw(getField())) {
+            updateScore()
             finishGame("draw")
         } else {
-            let winner = checkWin(getField())
             if(winner) {
+                if(winner === humanPlayer) {
+                    score.player++
+                } else {
+                    score.computer++
+                }
+                updateScore()
                 finishGame(winner)
             }
         }
         currentPlayer = computerPlayer === currentPlayer ? humanPlayer : computerPlayer
         return true
     } else return false
+}
+
+function updateScore() {
+    const scorePlayer1 = document.getElementById('player-score-value')
+    const scorePlayer2 = document.getElementById('computer-score-value')
+    
+    if(scorePlayer1.innerText != score.player) {
+        triggerAnimation(scorePlayer1)
+        setTimeout(() => {
+            scorePlayer1.innerText = score.player
+        }, 500)
+    }
+    if(scorePlayer2.innerText != score.computer) {
+        triggerAnimation(scorePlayer2)
+        setTimeout(() => {
+            scorePlayer2.innerText = score.computer
+        }, 500)
+    }
+}
+
+function triggerAnimation(element) {
+    element.classList.add('animate-change');
+    element.addEventListener('animationend', () => {
+        element.classList.remove('animate-change');
+    }, { once: true });
 }
